7e2b4f37982b751ec942e3abe9aa7b4edf424dd5
AldoRH <aldo.romeroh@gmail.com>
2024-02-10 21:37:28 -0600

    Create models
    Se importan todos los modelos en un solo archivo para facilitar la exportación de los modelos

---
+++ pelis_app/lib/models/models.dart
@@ -0,0 +1,5 @@
+export 'package:pelis_app/models/search_response.dart';
+export 'package:pelis_app/models/credits_response.dart';
+export 'package:pelis_app/models/popular_response.dart';
+export 'package:pelis_app/models/movie.dart';
+export 'package:pelis_app/models/now_playimg_response.dart';
